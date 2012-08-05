62294ad9154fab97c8cc8533115a4ce54fea6003
Lukas Walther <lukas@cast.li>
2012-08-05 18:14:30 +0200

    Added colors to players.

--- src/framework/Player.java
+++ src/framework/Player.java
@@ -1,5 +1,16 @@
 package framework;
 
+import org.newdawn.slick.Color;
+
 public enum Player {
 	Red,Blue,None;
+	
+	public Color getColor(){
+		switch(this){
+		case Red: return Color.red;
+		case Blue: return Color.blue;
+		case None: return Color.white;
+		}
+		return Color.black;
+	}
 }
