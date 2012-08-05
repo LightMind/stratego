e13afe9a8d5b6d670e522fb60a79673c897083c8
Lukas Walther <lukas@cast.li>
2012-08-05 16:33:46 +0200

    Added basic AppGameContainer Setup.

---
+++ src/basic/Test.java
@@ -0,0 +1,31 @@
+package basic;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Test extends BasicGame {
+
+	public Test(String title) {
+		super(title);
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+
+	}
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,23 @@
 package main;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Game;
+import org.newdawn.slick.SlickException;
+
+import basic.Test;
+
 public class Main{
 
 	/**
 	 * @param args
+	 * @throws SlickException 
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println(":)");
-		System.out.println("TEST BRANCH");
+	public static void main(String[] args) throws SlickException {
+		Game game = new Test("test");
+		AppGameContainer gc = new AppGameContainer(game, 800, 600, false);
+		gc.setTargetFrameRate(60);
+		gc.setAlwaysRender(true);
 
 	}
-
+	
 }
