14ffb7bf85980ed1e320d979afb059fe8f5d3b91
Helene <h@mail.dk>
2012-08-05 17:48:56 +0200

    visualizer draw terrain

--- src/basic/Test.java
+++ src/basic/Test.java
@@ -5,27 +5,31 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
+
 public class Test extends BasicGame {
 
 	public Test(String title) {
 		super(title);
 	}
 
-	@Override
-	public void render(GameContainer container, Graphics g)
-			throws SlickException {
-
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
-
+		// TODO Auto-generated method stub
+		
 	}
+
+
 
 }
