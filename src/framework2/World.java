e759c6bf1423adae6116b7da58158f7fdb951692
Lukas Walther <lukas@cast.li>
2012-08-11 15:47:02 +0200

    World is serializable.

--- src/framework2/World.java
+++ src/framework2/World.java
@@ -1,7 +1,9 @@
 package framework2;
 
+import java.io.Serializable;
 
-public interface World {
+
+public interface World extends Serializable{
 	public Unit getUnitAt(int column, int row);
 	public Unit getUnitAt(Location location);
 	public Terrain getTerrain(int column, int row);
